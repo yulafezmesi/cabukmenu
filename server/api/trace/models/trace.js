d122d5252bf5be4ad200822b15feb336643cfc36
Mert <certel@hotmail.com>
2020-07-17 00:07:22 +0300

    Initial commit

---
+++ server/api/trace/models/trace.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   afterCreate(result) {
+//     console.log("afterCreate");
+//   },
+//   // Called before an entry is created
+//   beforeCreate(data) {
+//     console.log("beforeCreate");
+//   },
+//   beforeFetchAll: async (model) => {
+//     console.log("beforeFetchAll: ");
+//   },
+
+//   // After fetching all values.
+//   // Fired after a `fetchAll` operation.
+//   afterFetchAll: async (model, results) => {
+//     console.log("afterFetchAll: ");
+//   },
+
+//   // Called after an entry is created
+// };
